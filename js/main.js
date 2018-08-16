8ec40098e28c3349544f405a0f94158d69f84452
encodedBicoding <dominicisioma000@gmail.com>
2018-08-16 12:57:26 +0100

    javascript folder added

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+// var x = ['a','b','c']
+
+// function y(v) {
+//      if(x.includes(v)){
+//          return (v + " is available")
+//      } else {
+//          return (v + " is not availbale")
+//      }
+//   }
+
+var cars = ["Volvo", "Benz", "Porsche", "Mazda"]
+
+ function carType(car) {
+     if (cars.includes(car)){
+         return (car + " is available");
+     } else {
+         return (car + "  is not available");
+     }
+ }
+
+
+
+
+
+
+
+
+
+
